5ea800f68309561c16103054764349ef98745c4a
d3j1x <d3j1x@yahoo.com>
2025-11-15 23:45:13 +0100

    update

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -86,7 +86,7 @@
 
     <div class="bg-white rounded-2xl shadow-lg hover:shadow-2xl transition overflow-hidden group">
       <div class="relative">
-        <img src={supportpcle_modern} alt="Product Image" class="w-full h-60 object-cover group-hover:scale-105 transition-transform" />
+        <img src={supportpcle_modern} alt="Product Image" class="w-full h-60 object-scale-down group-hover:scale-105 transition-transform" />
       </div>
       <div class="p-5">
         <h3 class="text-lg font-bold text-gray-900 mb-1">Support PCles Modern</h3>
@@ -114,7 +114,7 @@
 
     <div class="bg-white rounded-2xl shadow-lg hover:shadow-2xl transition overflow-hidden group">
       <div class="relative">
-        <img src={notebookmom} alt="Product Image" class="w-full h-60 object-cover group-hover:scale-105 transition-transform" />
+        <img src={notebookmom} alt="Product Image" class="w-full h-60 object-contain group-hover:scale-105 transition-transform" />
       </div>
       <div class="p-5">
         <h3 class="text-lg font-bold text-gray-900 mb-1">Notebook</h3>
